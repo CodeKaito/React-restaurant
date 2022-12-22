e94a609a33f504ca83484adab78ca9214d915273
Jerome Decinco <57111980+CodeKaito@users.noreply.github.com>
2022-12-22 11:07:47 +0100

    Add files via upload

---
+++ src/container/Findus/FindUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
+
+const FindUs = () => (
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Contact'/>
+      <h1 className='headtext__cormorant' style={{ marginBottom: '3rem'}}>
+        Find Us
+      </h1>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>Lane ends Bunga</p>
+        <p className='p__cormorant style={{ color: '#DCCA87'>Lane ends Bunga</p>
+        <p className='p__opensans'>Lane ends Bunga</p>
+      </div>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.findus} alt='findus'/>
+    </div>
+  </div>
+);
+
+export default FindUs;
